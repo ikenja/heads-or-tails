4aaf096edf43cf50a0f303069331139f528c21db
İrem Güven <53179694+ikenja@users.noreply.github.com>
2022-02-09 20:53:29 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+#Write your code below this line 👇
+#Hint: Remember to import the random module first. 🎲
+
+import random
+
+toss_value = random.randint(0,1)
+
+if toss_value == 1:
+  print("Heads")
+else:
+  print("Tails")
+
+
+
+
+
+
+
+
